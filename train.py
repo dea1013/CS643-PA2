4426b27eb5750d6cb0300aa445491b3d63c47438
Dilip Anand <89796039+dea1013@users.noreply.github.com>
2023-04-03 17:42:45 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,20 @@
+from pyspark.sql import SparkSession
 from pyspark.ml.classification import LogisticRegression
 
-dataset = spark.read.format("csv").\
-option('header', True).\
-option('inferSchema', True).\
-load("/home/dea1013/TrainingDataset.csv")
+if __name__ == '__main__':
 
-lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+        spark = SparkSession\
+                .builder\
+                .appName("train")\
+                .getOrCreate()
 
-lrModel = lr.fit(dataset)
+        df = spark.read.format("csv")\
+        .option('header', True)\
+        .option('inferSchema', True)\
+        .load("/home/dea1013/CS643-PA2/TrainingDataset.csv")
 
-print(lrModel.summary)
+        lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
+
+        lrModel = lr.fit(df)
+
+        print(lrModel.summary)
