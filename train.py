f693871333e629ff0abec474f2b6680bc39063e7
Dilip Anand <89796039+dea1013@users.noreply.github.com>
2023-04-09 15:48:37 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,42 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType
 
 if __name__ == '__main__':
 
+        # create spark session
         spark = SparkSession\
                 .builder\
                 .appName("train")\
                 .getOrCreate()
+        
+        # schema
+        schema = StructType([
+                StructField('"""""fixed acidity""""', DoubleType()),
+                StructField('""""volatile acidity""""', DoubleType()),
+                StructField('""""citric acid""""', DoubleType()),
+                StructField('""""residual sugar""""', DoubleType()),
+                StructField('""""chlorides""""', DoubleType()),
+                StructField('""""free sulfur dioxide""""', IntegerType()),
+                StructField('""""total sulfur dioxide""""', IntegerType()),
+                StructField('""""density""""', DoubleType()),
+                StructField('""""pH""""', DoubleType()),
+                StructField('""""sulphates""""', DoubleType()),
+                StructField('""""alcohol""""', DoubleType()),
+                StructField('""""quality"""""', IntegerType()),
+        ])
 
         df = spark.read.format("csv")\
+        .option('delimiter', ';')\
         .option('header', True)\
-        .option('inferSchema', True)\
+        .schema(schema)\
         .load("/home/dea1013/CS643-PA2/TrainingDataset.csv")
+
+        feature_cols = df.columns[:-1]
+        assembler = VectorAssembler(inputCols=feature_cols, outputCol='features')
+        df = assembler.transform(df).select('""""quality"""""','features')
+        df = df.withColumnRenamed('""""quality"""""', 'label')
 
         lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
